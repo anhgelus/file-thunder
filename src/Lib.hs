8028dde6b28a5b48022852091e495f06f571d4fd
Anhgelus Morhtuuzh <william@herges.fr>
2026-05-12 22:39:57 +0200

    feat(lib): split handling folders and files

--- src/Lib.hs
+++ src/Lib.hs
@@ -12,23 +12,36 @@ import Network.Mime (MimeType, defaultMimeMap, defaultMimeType)
 import Network.Wai
 
 app :: Application
-app req respond =
-    let path = pathInfo req
-     in respond $ responseFile status200 [(hContentType, getContentType path)] (T.unpack $ T.intercalate "/" path) Nothing
+app req respond = handle (pathInfo req) respond
+
+handle :: [T.Text] -> (Response -> IO ResponseReceived) -> IO ResponseReceived
+handle path respond
+    | (T.length p) == 0 = respond $ responseLBS status404 [] "not found"
+    | otherwise =
+        respond $
+            responseFile
+                status200
+                [(hContentType, getContentType path)]
+                (T.unpack $ T.intercalate "/" path)
+                Nothing
+  where
+    p = case unsnoc path of
+        Nothing -> ""
+        Just (_, v) -> v
 
 type Storage = Map.Map T.Text T.Text
 
 storageMiddleware :: T.Text -> Storage -> Middleware
 storageMiddleware def stor next req resp =
     -- remove empty parts
-    let base = filter (\t -> (T.length t) > 0) $ T.splitOn "/" def
+    let base = (filter (\t -> (T.length t) > 0) $ T.splitOn "/" def)
      in next
             req
                 { pathInfo = case uncons (pathInfo req) of
                     Just (key, t) -> case Map.lookup key stor of
                         Just v -> v : t
                         Nothing -> base ++ key : t
-                    Nothing -> base
+                    Nothing -> base ++ [""] -- normalize with trailing slash
                 }
             resp
 
