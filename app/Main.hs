8958ea3889c83680c799c5ae0f1921ff94c9cb6f
Anhgelus Morhtuuzh <william@herges.fr>
2026-05-12 22:18:43 +0200

    feat(lib): set content type

--- app/Main.hs
+++ app/Main.hs
@@ -7,11 +7,12 @@ module Main (main) where
 import Lib
 
 import Control.Exception (throw)
-import Data.List (intercalate)
+import Data.List (intercalate, uncons)
 import qualified Data.Map.Strict as Map
-import Data.Text (Text, pack)
+import qualified Data.Text as T
 import GHC.Generics (Generic)
 import Network.Wai.Handler.Warp
+import System.Environment (getArgs)
 import qualified Toml
 import Toml.Schema (
     FromValue,
@@ -21,18 +22,21 @@ import Toml.Schema (
     ToValue,
  )
 
-data Config = Config {root :: Text, storages :: Maybe [Storage]}
+data Config = Config {root :: T.Text, storages :: Maybe [Storage]}
     deriving (Eq, Show, Generic)
     deriving (ToTable, ToValue, FromValue) via GenericTomlTable Config
 
-data Storage = Storage {uri :: Text, path :: Text}
+data Storage = Storage {uri :: T.Text, path :: T.Text}
     deriving (Eq, Show, Generic)
     deriving (ToTable, ToValue, FromValue) via GenericTomlTable Storage
 
 main :: IO ()
 main = do
-    raw <- readFile "config.toml"
-    let res = loadConfig $ pack raw
+    args <- getArgs
+    raw <- readFile $ case uncons args of
+        Just (f, _) -> f
+        Nothing -> "/etc/thunderd.toml"
+    let res = loadConfig $ T.pack raw
     case warn res of
         Just w -> putStrLn $ intercalate "\n" w
         Nothing -> putStrLn "Config loaded"
@@ -41,13 +45,13 @@ main = do
 
 data ConfigRes = ConfigRes {config :: Config, warn :: Maybe [String]}
 
-loadConfig :: Text -> ConfigRes
+loadConfig :: T.Text -> ConfigRes
 loadConfig p = case Toml.decode p of
     Success [] v -> ConfigRes{config = v, warn = Nothing}
     Success w v -> ConfigRes{config = v, warn = Just w}
     Failure err -> throw $ userError $ intercalate "\n" err
 
-loadStorages :: Maybe [Storage] -> (Map.Map Text Text)
+loadStorages :: Maybe [Storage] -> (Map.Map T.Text T.Text)
 loadStorages stors = case stors of
     Just storage -> Map.fromList $ map (\s -> ((uri s), (path s))) storage
     Nothing -> Map.empty
--- src/Lib.hs
+++ src/Lib.hs
@@ -3,27 +3,40 @@ module Lib (
     storageMiddleware,
 ) where
 
-import Data.List (uncons)
+import Data.List (uncons, unsnoc)
 import qualified Data.Map.Strict as Map
-import Data.Text (Text)
+import qualified Data.Text as T
+import Network.HTTP.Types (hContentType)
 import Network.HTTP.Types.Status
+import Network.Mime (MimeType, defaultMimeMap, defaultMimeType)
 import Network.Wai
 
 app :: Application
-app req respond = do
-    print $ pathInfo req
-    respond $ responseLBS status200 [] "uwu"
+app req respond =
+    let path = pathInfo req
+     in respond $ responseFile status200 [(hContentType, getContentType path)] (T.unpack $ T.intercalate "/" path) Nothing
 
-type Storage = Map.Map Text Text
+type Storage = Map.Map T.Text T.Text
 
-storageMiddleware :: Text -> Storage -> Middleware
+storageMiddleware :: T.Text -> Storage -> Middleware
 storageMiddleware def stor next req resp =
-    next
-        req
-            { pathInfo = case uncons (pathInfo req) of
-                Just (key, t) -> case Map.lookup key stor of
-                    Just v -> v : t
-                    Nothing -> def : key : t
-                Nothing -> [def]
-            }
-        resp
+    -- remove empty parts
+    let base = filter (\t -> (T.length t) > 0) $ T.splitOn "/" def
+     in next
+            req
+                { pathInfo = case uncons (pathInfo req) of
+                    Just (key, t) -> case Map.lookup key stor of
+                        Just v -> v : t
+                        Nothing -> base ++ key : t
+                    Nothing -> base
+                }
+            resp
+
+getContentType :: [T.Text] -> MimeType
+getContentType path = case unsnoc path of
+    Nothing -> defaultMimeType
+    Just (_, f) -> case unsnoc $ T.splitOn "." f of
+        Nothing -> defaultMimeType
+        Just (_, ext) -> case Map.lookup ext defaultMimeMap of
+            Nothing -> defaultMimeType
+            Just t -> t
